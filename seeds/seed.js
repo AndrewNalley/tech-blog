ad3b09b376f2dddb50a600541351a1698284ec38
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-07-09 13:40:00 -0500

    add seed data

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+    for (const comment of commentData) {
+      await Comment.create({
+        ...comment,
+        blog_id: blog[Math.floor(Math.random() * blog.length)].id,
+      });
+    }
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
