123948596c5faaf3b0d8078c2fb23e9c7ec32c78
Andrew Nalley <awnalley@gmail.com>
2023-07-19 17:40:57 -0500

    add blog id to comment body

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,7 +7,7 @@ router.post('/', withAuth, async (req, res) => {
         const newComment = await Comment.create({
             ...req.body,
             user_id: req.session.user_id,
-            blog_id: this.blog.id
+            blog_id: blog_id,
         });
 
         res.status(200).json(newComment);
